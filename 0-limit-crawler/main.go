e076a1b4f26923781c2fbaebd563df238a730ebb
Vitor Guidi <vitor.guidi@vtex.com.br>
2023-10-28 16:07:44 -0300

    Implementing rate limiting with channel and time.ticker

--- 0-limit-crawler/main.go
+++ 0-limit-crawler/main.go
@@ -10,13 +10,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"sync"
+	"time"
 )
 
 // Crawl uses `fetcher` from the `mockfetcher.go` file to imitate a
 // real crawler. It crawls until the maximum depth has reached.
-func Crawl(url string, depth int, wg *sync.WaitGroup) {
+func Crawl(url string, depth int, wg *sync.WaitGroup, allowCrawlChannel *chan interface{}) {
 	defer wg.Done()
 
 	if depth <= 0 {
@@ -35,15 +37,33 @@ func Crawl(url string, depth int, wg *sync.WaitGroup) {
 	for _, u := range urls {
 		// Do not remove the `go` keyword, as Crawl() must be
 		// called concurrently
-		go Crawl(u, depth-1, wg)
+		go func() {
+			<-*allowCrawlChannel
+			fmt.Println(fmt.Sprintf("Crawling url %s at timestamp %s", url, time.Now()))
+			Crawl(u, depth-1, wg, allowCrawlChannel)
+		}()
 	}
 	return
 }
 
 func main() {
 	var wg sync.WaitGroup
+	allowCrawlChannel := make(chan interface{})
+	ticker := time.NewTicker(1 * time.Second)
+	ctx, cancel := context.WithCancel(context.Background())
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				break
+			case <-ticker.C:
+				allowCrawlChannel <- nil
+			}
+		}
+	}()
 
 	wg.Add(1)
-	Crawl("http://golang.org/", 4, &wg)
+	Crawl("http://golang.org/", 4, &wg, &allowCrawlChannel)
 	wg.Wait()
+	cancel()
 }
